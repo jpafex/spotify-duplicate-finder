e71938a41bec05c02132207285d5e84d3c78f5b3
jpafex <aparent9844@gmail.com>
2026-02-01 11:37:50 -0700

    Update cookies.py

--- afexcloud/cookies.py
+++ afexcloud/cookies.py
@@ -1,1 +1,13 @@
+import streamlit as st
+from streamlit_cookies_manager import EncryptedCookieManager
+from .config import COOKIE_PREFIX
+
+def get_cookies() -> EncryptedCookieManager:
+    cookies = EncryptedCookieManager(
+        prefix=COOKIE_PREFIX,
+        password=st.secrets.get("COOKIES_PASSWORD", "CHANGE_ME_SET_COOKIES_PASSWORD"),
+    )
+    if not cookies.ready():
+        st.stop()
+    return cookies
 
