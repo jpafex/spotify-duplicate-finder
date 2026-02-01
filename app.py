078ea9c1999e4e62eda2ccf145d41c8e8819ffda
jpafex <aparent9844@gmail.com>
2026-02-01 12:08:09 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from afexcloud.layout import bootstrap_page
 
+st.set_page_config(page_title="AfexCloud Dashboard", page_icon="☁️", layout="wide")
+
 bootstrap_page()
 
 st.title("☁️ AfexCloud")
