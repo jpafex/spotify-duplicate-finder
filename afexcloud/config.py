828e4f8225feb891d93aa1123089e560e1035376
jpafex <aparent9844@gmail.com>
2026-02-01 11:37:34 -0700

    Update config.py

--- afexcloud/config.py
+++ afexcloud/config.py
@@ -1,1 +1,8 @@
+APP_VERSION = "3.1.0"
+
+# Cookie keys
+AUTH_COOKIE_KEY = "afexcloud_logged_in_v1"
+SPOTIFY_TOKEN_COOKIE_KEY = "spotify_token_info_v1"
+SPOTIFY_STATE_COOKIE_KEY = "spotify_oauth_state_v1"
+COOKIE_PREFIX = "afexcloud/"
 
